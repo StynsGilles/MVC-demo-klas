b39adbc8c449a60cb785c0ba01e62f905d3d4d34
Gilles Styns <gilles.styns@student.howest.be>
2018-02-19 11:56:16 +0100

    lol

---
+++ MVC demo klas/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_demo_klas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string[] groenten = { "Rode kool", "Spruitjes", "Wortel", "Spinazie" };
+
+        public ViewResult Index(string name)
+        {
+            ViewBag.Groet = DateTime.Now.Hour < 12 ? "Goeiemorgen" : "Goeienamiddag";
+            return View();
+        }
+
+        public ViewResult Groenten(string zoekGroente)
+        {
+            ViewBag.Groenten = groenten;
+            if (!string.IsNullOrEmpty(zoekGroente))
+            {
+                ViewBag.Zoekresultaat = $"de gezochte groente is " +
+                                        $"de {Array.IndexOf(groenten, zoekGroente) + 1}e uit de lijst";
+            }
+            return View();
+        }
+    }
+}
